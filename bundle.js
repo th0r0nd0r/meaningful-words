bb4d8d0bde3113ee5330c855801a5a829f602fb8
= <pjfritch1@gmail.com>
2017-12-06 15:05:48 -0800

    implementing file import

--- bundle.js
+++ bundle.js
@@ -86,20 +86,32 @@ const writ = () => {document.write(" exports/imports working");};
 
 /* harmony default export */ __webpack_exports__["a"] = (writ);
 
-let fileList;
-let currentFile;
 
-const printFile = (event) => {
-  readFiles(event);
-  currentFile = fileList[0];
-  document.write("something happened");
-};
+const fileInput = document.getElementById("file-input");
 
-window.printFile = printFile;
 
-function readFiles(event) {
-  fileList = event.target.files;
-}
+
+
+
+
+
+
+
+
+// let fileList;
+// let currentFile;
+
+// const printFile = (event) => {
+//   readFiles(event);
+//   currentFile = fileList[0];
+//   document.write("something happened");
+// };
+
+// window.printFile = printFile;
+
+// function readFiles(event) {
+//   fileList = event.target.files;
+// }
 
 
 
--- test.js
+++ test.js
@@ -3,18 +3,30 @@ const writ = () => {document.write(" exports/imports working");};
 
 export default writ;
 
-let fileList;
-let currentFile;
 
-const printFile = (event) => {
-  readFiles(event);
-  currentFile = fileList[0];
-  document.write("something happened");
-};
+const fileInput = document.getElementById("file-input");
 
-window.printFile = printFile;
 
-function readFiles(event) {
-  fileList = event.target.files;
-}
+
+
+
+
+
+
+
+
+// let fileList;
+// let currentFile;
+
+// const printFile = (event) => {
+//   readFiles(event);
+//   currentFile = fileList[0];
+//   document.write("something happened");
+// };
+
+// window.printFile = printFile;
+
+// function readFiles(event) {
+//   fileList = event.target.files;
+// }
 
