c2c45bc32c2ba1976c0150ef2a3d6708392e6bfa
= <pjfritch1@gmail.com>
2017-12-07 13:22:15 -0800

    play around with bubbles

--- bubbles.js
+++ bubbles.js
@@ -45,3 +45,29 @@ var node = svg.selectAll('.node')
     .enter().append('circle')
     .attr('class', 'node')
     .attr('r', width * 0.03); //radius of circle
+
+
+function tick(e) {
+  node
+    .attr("cx", function(d) {
+      return d.x;
+    })
+    .attr("cy", function(d) {
+      return d.y;
+    })
+    .call(force.drag);
+
+  link
+    .attr("x1", function(d) {
+      return d.source.x;
+    })
+    .attr("y1", function(d) {
+      return d.source.y;
+    })
+    .attr("x2", function(d) {
+      return d.target.x;
+    })
+    .attr("y2", function(d) {
+      return d.target.y;
+    });
+}
